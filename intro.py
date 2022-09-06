7c78ff9c7597f65ca04b468f98318f631297a892
alcain <alejo.cain@capgemini.com>
2022-09-06 23:33:07 +0200

    Remove unused files

--- intro.py
+++
@@ -1,9 +0,0 @@
-from model import find_index, test
-import sys
-
-courses = ['History', 'Math', 'Physics', 'CompSci']
-
-index = find_index(courses, 'Math')
-# print(index)
-# print(test)
-print(sys.path)
