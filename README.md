f8e1e9b134abfc347e6c403bc7cc2c6e0c7e3bb6
Adam Mokhtar <adammokhtar@Adams-MacBook-Pro.local>
2022-09-06 21:43:33 +0200

    added the new GUI and updated the readme

--- README.md
+++ README.md
@@ -10,11 +10,58 @@ Copy the repository with the source code on your machine.
 
 ## GUI
 
+**Need to run:**
+```sh
+pip install tk 
+pip install Pillow
+```
+### Explanation
+This section focuses on creating the interface for the user.
+For the development of the GUI TKinter was used which is a Python toolkit.
+For the approach of creation, Tkinter Frame was used which helps with grouping and organizing widgets on the GUI.
+Following is a breakdown of the important sections of the code:
+
+### Comparison frame
+This frame is located on the right-hand side of the screen with one column and three rows. Each row includes a canvas that will hold images.
+These images are resized to a specific size to fit the size of the canvas.
+ 
+### Attributes frame
+Located on the top left side of the page with one column and two rows.
+In each row of these frames, there is a label representing an attribute.
+ 
+ 
+###  Main Canvas
+This canvas is in the center of the page. Which represents the image taken.
+ 
+### Btn frame
+This frame is on the center bottom of the screen. The frame has one row and two columns. Each column represents a button. Button RESET which is intended to reset the application. Button TAKE A PIC! Is intended to be used to capture images.
+ 
+### Callable functions
+**SetThreePics** 
+This function aims to set the three compression images by passing in the three paths of the images which will be set accordingly. Each image passed will be resized to fit the canvas and afterward published.
+
+> Note: change setThreePics to array parameter later!
+
+**setMainFrame**
+This method will set the image on the main canvas. As a parameter, the path of the main image will be passed. Further, the image will be resized and published on the main canvas.
+ 
+**setAtrributes**
+setAttributes fills in the two labels of the attributes frame.  As the first parameter, a string of the emotion result is expected. while the second parameter expects the label and the result of the second attribute.
+ 
+**rest**
+This function resets the images on the comparison frame and the main canvas to the blue blank image.
+ 
+ 
+**dummpySetImagesAndAttr**
+This function sets some dummy data for the images on the comparison frame and the main canvas. Similarly, for the attributes, some dummy data will be filled.
+
+
+
 ## Model
 
 ## Developers
 
-How setup 09-08-2022
+How setup 06-09-2022
 
 In your command line run the command:
 
--- gui.py
+++ gui.py
@@ -1,28 +1,135 @@
-# TODO: frontend code to showcase pictures
-# input: 4 pictures, percentage, attributes
-# TODO: reset button
+import tkinter as tk
+from tkinter import TOP, Canvas
+from PIL import ImageTk,Image  
 
-# Import the required libraries
-from tkinter import *
-from tkinter import messagebox
-from tkinter import ttk
+window = tk.Tk()
 
-# Create an instance of tkinter frame
-win = Tk()
+# window properties
+window.geometry('1080x1920')
+window.title("Beat The Algorithm")
+#importing image
+imgPathDummy = "images/img.png"
+imagPathDummy2 = "images/blue.png"
+imagPathDummy3 = "images/main.png"
 
-# Set the size of the tkinter window
-win.geometry("700x350")
+titleLabel = tk.Label(window, text="BEAT THE ALGORITHM",font=('Arial 20 bold italic'))
+titleLabel.pack( padx=20, pady=20)
 
-# Define a function to show the popup message
+# comparison frame
+comFrame = tk.Frame(window)
+comFrame.columnconfigure(0,weight=0)
+#comparison canvases
+#1
+comCanvas1 = Canvas(comFrame, width = 400, height = 300) 
+comCanvas1.grid(row=0,column=0)
+img1Input= (Image.open(imagPathDummy2))
+resized_image11= img1Input.resize((400,300), Image.ANTIALIAS)
+blankImage= ImageTk.PhotoImage(resized_image11)
+comCanvas1_container = comCanvas1.create_image(10, 10, anchor=tk.NW, image=blankImage) 
+#2
+comCanvas2 = Canvas(comFrame, width = 400, height = 300) 
+comCanvas2.grid(row=1,column=0)
+comCanvas2_container = comCanvas2.create_image(10, 10, anchor=tk.NW, image=blankImage) 
+#3
+comCanvas3 = Canvas(comFrame, width = 400, height = 300) 
+comCanvas3.grid(row=2,column=0)
+comCanvas1_container = comCanvas3.create_image(10, 10, anchor=tk.NW, image=blankImage) 
 
+comFrame.pack(padx=20, side=tk.RIGHT)
 
-def show_msg():
-    messagebox.showinfo("Message", "Hey There! I hope you are doing well.")
+# Attributes frame
+attrFrame = tk.Frame(window)
+attrFrame.columnconfigure(0)
 
+#pic attributes labels
+emoLabel = tk.Label(attrFrame, text="Emotion: N/A",font=('Arial 18'), bd=1, relief= "solid")
+emoLabel.grid(row=0,column=0,sticky="W",pady=10)
+# titleLabel.pack(side=tk.LEFT, ipadx=10, ipady=10)
 
-# Add an optional Label widget
-Label(win, text="Welcome BTA!", font=('Aerial 17 bold italic')).pack(pady=30)
+secondLabel = tk.Label(attrFrame, text="Smt: N/A",font=('Arial 18'), bd=1, relief= "solid")
+secondLabel.grid(row=1,column=0,sticky="W",pady=10)
+# titleLabel.pack(side=tk.LEFT,)
 
-# Create a Button to display the message
-ttk.Button(win, text="Click Here", command=show_msg).pack(pady=20)
-win.mainloop()
+attrFrame.pack(anchor = "w", side=TOP,padx=10)
+
+#main canvas
+mainCanvas = Canvas(window, width = 900, height = 700) 
+mainCanvas.pack(padx=(400, 0)) #padx=(250, 0)
+img1Input= (Image.open(imagPathDummy2))
+#Resize the Image using resize method
+resizedMain_image = img1Input.resize((900,700), Image.ANTIALIAS)
+newMain_image= ImageTk.PhotoImage(resizedMain_image)
+mainCanvas_container = mainCanvas.create_image(10, 10, anchor=tk.NW, image=newMain_image) 
+
+def setThreePics(Img1Path,Img2Path,Img3Path):
+    #1
+    #Resize the Image using resize method
+    img1InputFun= (Image.open(Img1Path))
+    resized_image1= img1InputFun.resize((400,300), Image.ANTIALIAS)
+    new_image1= ImageTk.PhotoImage(resized_image1)
+    comCanvas1.itemconfig(comCanvas1_container,image = new_image1)
+    comCanvas1.imgref = new_image1
+    #2
+    img2InputFun= (Image.open(Img2Path))
+    resized_image2= img2InputFun.resize((400,300), Image.ANTIALIAS)
+    new_image2= ImageTk.PhotoImage(resized_image2)
+    comCanvas2.itemconfig(comCanvas1_container,image = new_image2)
+    comCanvas2.imgref = new_image2
+    # #3
+    img3InputFun= (Image.open(Img3Path))
+    resized_image3 = img3InputFun.resize((400,300), Image.ANTIALIAS)
+    new_image3= ImageTk.PhotoImage(resized_image3)
+    comCanvas3.itemconfig(comCanvas1_container,image = new_image3)
+    comCanvas3.imgref = new_image3
+
+def setMainFrame(ImgPath):
+    imgInputFun= (Image.open(ImgPath))
+    resizedMain_image = imgInputFun.resize((900,700), Image.ANTIALIAS)
+    newMain_image = ImageTk.PhotoImage(resizedMain_image)
+    mainCanvas.itemconfig(mainCanvas_container,image = newMain_image)
+    mainCanvas.imgref = newMain_image
+
+def dummpySetImagesAndAttr():
+    setMainFrame(imagPathDummy3)
+    setThreePics(imgPathDummy,imgPathDummy,imgPathDummy)
+    setAttributes("Happy","N/A: N/A")
+
+def reset():
+    #set main frame to defult
+    img1Input= (Image.open(imagPathDummy2))
+    blankImage= ImageTk.PhotoImage(img1Input)
+    mainCanvas.itemconfig(mainCanvas_container,image = blankImage)
+    mainCanvas.imgref = blankImage
+    #set all three pics to defult
+    #1
+    comCanvas1.itemconfig(comCanvas1_container,image = blankImage)
+    comCanvas1.imgref = blankImage
+    #2
+    comCanvas2.itemconfig(comCanvas1_container,image = blankImage)
+    comCanvas2.imgref = blankImage
+    #3
+    comCanvas3.itemconfig(comCanvas1_container,image = blankImage)
+    comCanvas3.imgref = blankImage
+
+    emoLabel['text'] = "Emotion:"
+    secondLabel['text'] = "Smt: N/A"
+
+def setAttributes(emotionAttr,secondFullAttr):
+    emoLabel['text'] = "Emotion: "+emotionAttr
+    secondLabel['text'] = secondFullAttr
+
+# btn frame
+btnFrame = tk.Frame(window)
+btnFrame.columnconfigure(0,weight=1)
+#reset btn
+#1
+resetBtn = tk.Button(btnFrame, text="RESET", font=('Arial 10'),command=lambda:reset())
+resetBtn.grid(row=0,column=0)
+#take a pic btn
+#2
+takePicBtn = tk.Button(btnFrame, text="TAKE A PIC!", font=('Arial 16 bold'),command=lambda:dummpySetImagesAndAttr())
+takePicBtn.grid(row=0,column=1)
+
+btnFrame.pack(side=tk.BOTTOM, ipadx=10, ipady=10)
+
+window.mainloop()
